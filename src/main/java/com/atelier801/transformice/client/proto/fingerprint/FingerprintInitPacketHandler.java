62c0c9d32eeb6e6b8afde211f45c114eb829db33
Stanislav Izmalkov <izstas@live.ru>
2014-08-01 12:55:33 +0400

    Added a bit of logging to FingerprintInitPacketHandler

--- src/main/java/com/atelier801/transformice/client/proto/fingerprint/FingerprintInitPacketHandler.java
+++ src/main/java/com/atelier801/transformice/client/proto/fingerprint/FingerprintInitPacketHandler.java
@@ -2,8 +2,12 @@ package com.atelier801.transformice.client.proto.fingerprint;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class FingerprintInitPacketHandler extends SimpleChannelInboundHandler<FingerprintInitPacket> {
+    private static final Logger logger = LoggerFactory.getLogger(FingerprintInitPacketHandler.class);
+
     private final FingerprintGenerator generator;
 
     public FingerprintInitPacketHandler(FingerprintGenerator generator) {
@@ -14,6 +18,7 @@ public final class FingerprintInitPacketHandler extends SimpleChannelInboundHand
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, FingerprintInitPacket msg) throws Exception {
         generator.init(msg.getFingerprintCounter());
+        logger.debug("Fingerprint generator has been initialized with counter = {}", msg.getFingerprintCounter());
 
         ctx.fireChannelRead(msg);
     }
