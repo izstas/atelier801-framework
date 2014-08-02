e80f856cb0a7289803f875a9d8dc4e12d611d0b1
Stanislav Izmalkov <izstas@live.ru>
2014-08-02 15:39:32 +0400

    Added packet for entering tribe house

---
+++ src/main/java/com/atelier801/transformice/client/proto/packet/out/OPTribeHouse.java
@@ -0,0 +1,19 @@
+package com.atelier801.transformice.client.proto.packet.out;
+
+import com.google.common.base.Objects;
+
+import com.atelier801.transformice.client.proto.TransformiceByteBuf;
+
+// Valid for 1.180
+@OutboundPacket.Code(major = 16, minor = 1)
+public final class OPTribeHouse implements OutboundPacket {
+    @Override
+    public void write(TransformiceByteBuf out) {
+        // Empty
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this).toString();
+    }
+}
