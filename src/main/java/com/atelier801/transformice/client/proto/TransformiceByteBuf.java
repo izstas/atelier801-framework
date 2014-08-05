d264e986a44188e91ac3675e3b2be7e4f38d9c55
Stanislav Izmalkov <izstas@live.ru>
2014-08-05 12:36:46 +0400

    Added some more useful methods to TransformicecByteBuf

--- src/main/java/com/atelier801/transformice/client/proto/TransformiceByteBuf.java
+++ src/main/java/com/atelier801/transformice/client/proto/TransformiceByteBuf.java
@@ -1,8 +1,10 @@
 package com.atelier801.transformice.client.proto;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import com.google.common.collect.ImmutableList;
+import com.google.common.primitives.Bytes;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.WrappedByteBuf;
 import io.netty.util.CharsetUtil;
@@ -27,6 +29,21 @@ public class TransformiceByteBuf extends WrappedByteBuf {
         readBytes(data);
 
         return new String(data, CharsetUtil.UTF_8);
+    }
+
+    public String readUTFBytes(int length) {
+        if (length < 0) {
+            throw new IllegalArgumentException("length must not be negative");
+        }
+
+        byte[] data = new byte[length];
+        readBytes(data);
+
+        return new String(data, 0, Bytes.indexOf(data, (byte) 0), CharsetUtil.UTF_8);
+    }
+
+    public <T> List<T> readList(int length, Function<TransformiceByteBuf, T> reader) {
+        return readList(length, () -> reader.apply(this));
     }
 
     public <T> List<T> readList(int length, Supplier<T> reader) {
