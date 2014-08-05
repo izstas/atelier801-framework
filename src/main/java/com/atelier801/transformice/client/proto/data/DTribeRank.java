e7e62657420fa97e09aa3e333f79113c6e532ac0
Stanislav Izmalkov <izstas@live.ru>
2014-08-05 13:04:17 +0400

    Added tribe info packet

---
+++ src/main/java/com/atelier801/transformice/client/proto/data/DTribeRank.java
@@ -0,0 +1,49 @@
+package com.atelier801.transformice.client.proto.data;
+
+import java.util.List;
+import com.google.common.base.Objects;
+
+import com.atelier801.transformice.client.proto.TransformiceByteBuf;
+
+// Valid for 1.180
+public final class DTribeRank {
+    private final int id;
+    private final String name;
+    private final int position;
+    private final List<Boolean> permissions;
+
+    public DTribeRank(TransformiceByteBuf in) {
+        id = in.readInt();
+        name = in.readUTFBytes(20);
+        in.readBoolean();
+        in.readInt();
+        position = in.readByte();
+        permissions = in.readList(in.readShort(), in::readBoolean);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public List<Boolean> getPermissions() {
+        return permissions;
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("id", id)
+                .add("name", name)
+                .add("position", position)
+                .add("permissions", permissions)
+                .toString();
+    }
+}
---
+++ src/main/java/com/atelier801/transformice/client/proto/packet/in/IPTribe.java
@@ -0,0 +1,58 @@
+package com.atelier801.transformice.client.proto.packet.in;
+
+import java.util.List;
+import com.google.common.base.Objects;
+
+import com.atelier801.transformice.client.proto.TransformiceByteBuf;
+import com.atelier801.transformice.client.proto.data.DTribeRank;
+
+// Valid for 1.180
+@InboundTribullePacket.Label("ET_ResultatInformationsTribu")
+public final class IPTribe implements InboundTribullePacket {
+    private final int id;
+    private final String name;
+    private final String greeting;
+    private final int houseMap;
+    private final List<DTribeRank> ranks;
+
+    public IPTribe(TransformiceByteBuf in) {
+        in.readInt();
+        id = in.readInt();
+        name = in.readUTFBytes(50);
+        greeting = in.readUTF();
+        houseMap = in.readInt();
+        in.readInt();
+        ranks = in.readList(in.readShort(), DTribeRank::new);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public int getHouseMap() {
+        return houseMap;
+    }
+
+    public List<DTribeRank> getRanks() {
+        return ranks;
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("id", id)
+                .add("name", name)
+                .add("greeting", greeting)
+                .add("houseMap", houseMap)
+                .add("ranks", ranks)
+                .toString();
+    }
+}
