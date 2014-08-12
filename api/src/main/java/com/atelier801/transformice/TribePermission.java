699c3af3a53d55bd63f7206cfd8459499a8e5d02
Stanislav Izmalkov <izstas@live.ru>
2014-08-12 16:51:37 +0400

    Turns out zero index of permissions is actually unused...

--- api/src/main/java/com/atelier801/transformice/TribePermission.java
+++ api/src/main/java/com/atelier801/transformice/TribePermission.java
@@ -10,15 +10,15 @@ import java.util.Map;
 public final class TribePermission {
     private static final Map<Integer, TribePermission> byId = new HashMap<>();
 
-    public static final TribePermission LEADER = new TribePermission(0, "Leader");
-    public static final TribePermission CHANGE_GREETING = new TribePermission(1, "Can change the greeting message");
-    public static final TribePermission CHANGE_RANKS = new TribePermission(2, "Can edit ranks");
-    public static final TribePermission ASSIGN_RANKS = new TribePermission(3, "Can change the members' ranks");
-    public static final TribePermission INVITE_MEMBERS = new TribePermission(4, "Can invite new members");
-    public static final TribePermission KICK_MEMBERS = new TribePermission(5, "Can exclude members");
-    public static final TribePermission PLAY_MUSIC = new TribePermission(6, "Can play music");
-    public static final TribePermission CHANGE_HOUSE_MAP = new TribePermission(7, "Can change Tribe House map");
-    public static final TribePermission PLAY_MAPS = new TribePermission(8, "Can load a map");
+    public static final TribePermission LEADER = new TribePermission(1, "Leader");
+    public static final TribePermission CHANGE_GREETING = new TribePermission(2, "Can change the greeting message");
+    public static final TribePermission CHANGE_RANKS = new TribePermission(3, "Can edit ranks");
+    public static final TribePermission ASSIGN_RANKS = new TribePermission(4, "Can change the members' ranks");
+    public static final TribePermission INVITE_MEMBERS = new TribePermission(5, "Can invite new members");
+    public static final TribePermission KICK_MEMBERS = new TribePermission(6, "Can exclude members");
+    public static final TribePermission PLAY_MUSIC = new TribePermission(7, "Can play music");
+    public static final TribePermission CHANGE_HOUSE_MAP = new TribePermission(8, "Can change Tribe House map");
+    public static final TribePermission PLAY_MAPS = new TribePermission(9, "Can load a map");
 
 
     private final int id;
