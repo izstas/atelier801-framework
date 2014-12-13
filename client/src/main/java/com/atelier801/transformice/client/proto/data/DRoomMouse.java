26897149a92a954fae1aceefee4f15da35ecbba9
Stanislav Izmalkov <izstas@live.ru>
2014-12-14 01:05:32 +0300

    Added more data to DRoomMouse: dead flag, score, cheese flag

--- client/src/main/java/com/atelier801/transformice/client/proto/data/DRoomMouse.java
+++ client/src/main/java/com/atelier801/transformice/client/proto/data/DRoomMouse.java
@@ -4,12 +4,15 @@ import java.util.List;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Splitter;
 
-// Valid for 1.201
+// Valid for 1.212
 public final class DRoomMouse {
     private static final Splitter splitter = Splitter.on('#');
 
     private final String name;
     private final int id;
+    private final boolean dead;
+    private final int score;
+    private final boolean cheese;
 
     public DRoomMouse(String in) {
         this(splitter.splitToList(in));
@@ -18,6 +21,10 @@ public final class DRoomMouse {
     public DRoomMouse(List<String> in) {
         name = in.get(0);
         id = Integer.parseInt(in.get(1));
+        // TFM also checks if the field below is <= 2 and doesn't add id -> mouse mapping if it's not?
+        dead = Integer.parseInt(in.get(3)) > 0;
+        score = Integer.parseInt(in.get(4));
+        cheese = in.get(5).equals("1");
     }
 
     public String getName() {
@@ -28,11 +35,26 @@ public final class DRoomMouse {
         return id;
     }
 
+    public boolean isDead() {
+        return dead;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public boolean hasCheese() {
+        return cheese;
+    }
+
     @Override
     public String toString() {
         return MoreObjects.toStringHelper(this)
                 .add("name", name)
                 .add("id", id)
+                .add("dead", dead)
+                .add("score", score)
+                .add("cheese", cheese)
                 .toString();
     }
 }
