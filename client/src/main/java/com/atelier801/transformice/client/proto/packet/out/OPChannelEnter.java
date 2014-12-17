5df401777ebd496fd5a12ae3f4d000be36dc257d
Stanislav Izmalkov <izstas@live.ru>
2014-12-17 21:09:27 +0300

    Added new outbound packets for chat channels

--- client/src/main/java/com/atelier801/transformice/client/proto/packet/out/OPChannelEnter.java
+++ client/src/main/java/com/atelier801/transformice/client/proto/packet/out/OPChannelEnter.java
@@ -5,16 +5,14 @@ import lombok.*;
 import com.atelier801.transformice.client.proto.TransformiceByteBuf;
 
 // Valid for 1.213
-@OutboundTribullePacket.Label("ST_EnvoitMessageCanal")
+@OutboundTribullePacket.Label("ST_RejoindreCanal")
 @AllArgsConstructor @ToString
-public final class OPChannelMessage extends OutboundReferencedTribullePacket {
-    private final int channelId;
-    private final String message;
+public final class OPChannelEnter extends OutboundReferencedTribullePacket {
+    private final String name;
 
     @Override
     public void write(TransformiceByteBuf out) {
         out.writeInt(referenceId);
-        out.writeInt(channelId);
-        out.writeUTF(message);
+        out.writeUTF(name);
     }
 }
