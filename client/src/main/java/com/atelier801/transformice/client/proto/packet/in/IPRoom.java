0fe7fdd3ebafe742c3f20882b6f8c150a7413701
Stanislav Izmalkov <izstas@live.ru>
2014-12-29 18:13:11 +0300

    Updated IPRoom for 1.215

--- client/src/main/java/com/atelier801/transformice/client/proto/packet/in/IPRoom.java
+++ client/src/main/java/com/atelier801/transformice/client/proto/packet/in/IPRoom.java
@@ -1,26 +1,16 @@
 package com.atelier801.transformice.client.proto.packet.in;
 
-import com.google.common.base.MoreObjects;
-
+import lombok.*;
 import com.atelier801.transformice.client.proto.TransformiceByteBuf;
 
-// Valid for 1.180
+// Valid for 1.215
 @InboundPacket.Code(major = 5, minor = 21)
+@Getter @ToString
 public final class IPRoom implements InboundPacket {
     private final String room;
 
     public IPRoom(TransformiceByteBuf in) {
+        in.readBoolean();
         room = in.readUTF();
-    }
-
-    public String getRoom() {
-        return room;
-    }
-
-    @Override
-    public String toString() {
-        return MoreObjects.toStringHelper(this)
-                .add("room", room)
-                .toString();
     }
 }
