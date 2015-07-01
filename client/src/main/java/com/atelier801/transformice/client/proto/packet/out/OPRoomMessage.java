c3f944e4db8868bcd10511bcef23a814529b24b9
Stanislav Izmalkov <izstas@live.ru>
2015-07-01 22:02:47 +0300

    Update for 1.254: Packet for sending room chat messages became (6, 7) instead of (6, 8) all of a sudden

--- client/src/main/java/com/atelier801/transformice/client/proto/packet/out/OPRoomMessage.java
+++ client/src/main/java/com/atelier801/transformice/client/proto/packet/out/OPRoomMessage.java
@@ -1,28 +1,17 @@
 package com.atelier801.transformice.client.proto.packet.out;
 
-import com.google.common.base.MoreObjects;
+import lombok.*;
 
 import com.atelier801.transformice.client.proto.TransformiceByteBuf;
 
-// Valid for 1.180
-@OutboundPacket.Code(major = 6, minor = 8)
+// Valid for 1.254
+@OutboundPacket.Code(major = 6, minor = 7)
+@AllArgsConstructor @ToString
 public final class OPRoomMessage implements OutboundPacket {
     private final String message;
-
-    public OPRoomMessage(String message) {
-        this.message = message;
-    }
 
     @Override
     public void write(TransformiceByteBuf out) {
         out.writeUTF(message);
-        out.writeByte(32);
-    }
-
-    @Override
-    public String toString() {
-        return MoreObjects.toStringHelper(this)
-                .add("message", message)
-                .toString();
     }
 }
